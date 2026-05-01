d7bd3a2a9725b54c70195a14620f12fc51a8ef66
pyroliso <liselepinay@orange.fr>
2026-05-01 23:27:01 +0200

    Implement date and time update functionality
    Added a function to update the current date and time, and set intervals for updates.

--- script.js
+++ script.js
@@ -137,3 +137,37 @@ function updateFooter() {
 
 setInterval(updateFooter, 1000);
 updateFooter();
+
+function updateDateTime() {
+  const now = new Date();
+  const hour = now.getHours();
+
+  const date = now.toLocaleDateString("fr-FR");;
+  const time = now.toLocaleTimeString("fr-FR");
+
+  document.getElementById("datetime").textContent =
+    date + ", il est " + time + "." ;
+
+  document.body.classList.remove(
+    "morning",
+    "noon",
+    "sunset",
+    "night"
+  );
+
+  if (hour >= 6 && hour < 11) {
+    document.body.classList.add("morning");
+  } 
+  else if (hour >= 11 && hour < 18) {
+    document.body.classList.add("noon");
+  } 
+  else if (hour >= 18 && hour < 22) {
+    document.body.classList.add("sunset");
+  } 
+  else {
+    document.body.classList.add("night");
+  }
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
